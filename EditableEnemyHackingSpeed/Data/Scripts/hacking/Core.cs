3980d89971c05d4819ef7336fb8e70ad7232ca89
Gauge <Gauger16@gmail.com>
2024-07-01 12:40:02 -0700

    Added editable enemy hacking speed

---
+++ EditableEnemyHackingSpeed/Data/Scripts/hacking/Core.cs
@@ -0,0 +1,96 @@
+﻿using Sandbox.Game.Entities;
+using Sandbox.Game.Weapons;
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Weapons;
+using System.Collections.Generic;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+
+namespace BalancedHacking
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class Core : MySessionComponentBase
+    {
+        private Settings config = null;
+
+        public override void Init(MyObjectBuilder_SessionComponent sessionComponent)
+        {
+            config = Settings.Load();
+            MyAPIGateway.Session.DamageSystem.RegisterBeforeDamageHandler(0, Handler);
+        }
+
+        private void Handler(object target, ref MyDamageInformation info)
+        {
+            IMySlimBlock block = target as IMySlimBlock;
+            if (block == null) return; // End: Only reduce damage to blocks
+
+            IMyEntity ent = MyAPIGateway.Entities.GetEntityById(info.AttackerId);
+            if (ent == null) return; // End: Must be a player character
+
+            IMyHandheldGunObject<MyToolBase> tool = null;
+            IMyHandheldGunObject<MyGunBase> gun = null;
+
+            if (info.Type == MyDamageType.Grind)
+            {
+                tool = ent as IMyAngleGrinder;
+            }
+            else if (info.Type == MyDamageType.Drill)
+            {
+                tool = ent as IMyHandDrill;
+            }
+            else
+            {
+                gun = ent as IMyAutomaticRifleGun;
+            }
+
+            if (tool == null && gun == null) return; // End: damage must be done by tool or hand weapon
+
+            bool isTool = tool != null;
+            long identity = (isTool) ? tool.OwnerIdentityId : gun.OwnerIdentityId;
+
+            List<IMyPlayer> players = new List<IMyPlayer>();
+            MyAPIGateway.Players.GetPlayers(players, p => p.IdentityId == identity);
+            if (players.Count == 0 || // End: Player not found. this can happen if the character is an NPC
+                block.CubeGrid.BigOwners.Count == 0) return;  // End: Unowned structures take full damage
+
+            MyRelationsBetweenPlayerAndBlock relation = players[0].GetRelationTo(block.CubeGrid.BigOwners[0]);
+            if (!(relation == MyRelationsBetweenPlayerAndBlock.Enemies || 
+                relation == MyRelationsBetweenPlayerAndBlock.Neutral)) return; // End: Friendly and unowned structures take full damage
+
+            if (block.FatBlock != null && block.FatBlock is IMyTerminalBlock)
+            {
+                if (block.FatBlock.OwnerId != 0)
+                {
+                    info.Amount = info.Amount * (1 + (1 - MyAPIGateway.Session.HackSpeedMultiplier)) * GetTerminalBlockHackSpeedAboveFunctional(isTool);
+                }
+                else
+                {
+                    info.Amount = info.Amount * GetTerminalBlockHackSpeedBelowFunctional(isTool);
+                }
+
+            }
+            else
+            {
+                info.Amount = info.Amount * GetNonTerminalBlockHackSpeed(isTool);
+            }
+        }
+
+        private float GetTerminalBlockHackSpeedAboveFunctional(bool isTool)
+        {
+            return (isTool) ? config.TerminalBlockHackSpeedAboveFunctional : config.HandGunTerminalBlockHackSpeedAboveFunctional;
+        }
+
+        private float GetTerminalBlockHackSpeedBelowFunctional(bool isTool)
+        {
+            return (isTool) ? config.TerminalBlockHackSpeedBelowFunctional : config.HandGunTerminalBlockHackSpeedBelowFunctional;
+        }
+
+        private float GetNonTerminalBlockHackSpeed(bool isTool)
+        {
+            return (isTool) ? config.NonTerminalBlockHackSpeed : config.HandGunNonTerminalBlockHackSpeed;
+        }
+
+    }
+}
---
+++ EditableEnemyHackingSpeed/Data/Scripts/hacking/Settings.cs
@@ -0,0 +1,89 @@
+﻿using ProtoBuf;
+using Sandbox.ModAPI;
+using System;
+using System.IO;
+using VRage.Utils;
+
+namespace BalancedHacking
+{
+    [ProtoContract]
+    public class Settings
+    {
+        private const string Filename = "BalancedHackingSpeed.cfg";
+
+        public static readonly Settings Default = new Settings
+        {
+            TerminalBlockHackSpeedAboveFunctional = 0.15f,
+            TerminalBlockHackSpeedBelowFunctional = 0.2f,
+            NonTerminalBlockHackSpeed = 0.04f,
+
+            HandGunTerminalBlockHackSpeedAboveFunctional = 1f,
+            HandGunTerminalBlockHackSpeedBelowFunctional = 1f,
+            HandGunNonTerminalBlockHackSpeed = 0.5f
+        };
+
+        [ProtoMember(1)]
+        public float TerminalBlockHackSpeedAboveFunctional { get; set; }
+        [ProtoMember(2)]
+        public float TerminalBlockHackSpeedBelowFunctional { get; set; }
+        [ProtoMember(3)]
+        public float NonTerminalBlockHackSpeed { get; set; }
+        [ProtoMember(4)]
+        public float HandGunTerminalBlockHackSpeedAboveFunctional { get; set; }
+        [ProtoMember(5)]
+        public float HandGunTerminalBlockHackSpeedBelowFunctional { get; set; }
+        [ProtoMember(6)]
+        public float HandGunNonTerminalBlockHackSpeed { get; set; }
+
+
+        public static Settings Load()
+        {
+            Settings s = null;
+            try
+            {
+                if (MyAPIGateway.Utilities.FileExistsInWorldStorage(Filename, typeof(Settings)))
+                {
+                    MyLog.Default.Info("[BalancedHackingSpeed] Loading saved settings");
+                    TextReader reader = MyAPIGateway.Utilities.ReadFileInWorldStorage(Filename, typeof(Settings));
+                    string text = reader.ReadToEnd();
+                    reader.Close();
+
+                    s = MyAPIGateway.Utilities.SerializeFromXML<Settings>(text);
+                }
+                else
+                {
+                    MyLog.Default.Info("[BalancedHackingSpeed] Config file not found. Loading default settings");
+                    s = Default;
+                    Save(s);
+                }
+            }
+            catch (Exception e)
+            {
+                MyLog.Default.Warning($"[BalancedHackingSpeed] Failed to load saved configuration. Loading defaults\n {e.ToString()}");
+                s = Default;
+                Save(s);
+            }
+
+            Save(s);
+            return s;
+        }
+
+        public static void Save(Settings settings)
+        {
+            if (MyAPIGateway.Session.IsServer)
+            {
+                try
+                {
+                    MyLog.Default.Info("[BalancedHackingSpeed] Saving Settings");
+                    TextWriter writer = MyAPIGateway.Utilities.WriteFileInWorldStorage(Filename, typeof(Settings));
+                    writer.Write(MyAPIGateway.Utilities.SerializeToXML(settings));
+                    writer.Close();
+                }
+                catch (Exception e)
+                {
+                    MyLog.Default.Error($"[BalancedHackingSpeed] Failed to save settings\n{e.ToString()}");
+                }
+            }
+        }
+    }
+}
