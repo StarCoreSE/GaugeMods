c9fa15a89a6ce0e2ab70e6c3cfc7f63a14937c32
Gauge <Gauger16@gmail.com>
2024-07-01 18:49:45 -0700

    Initial commit of the grapple hook

---
+++ GrappleHook/Data/Scripts/hook/Tools.cs
@@ -0,0 +1,202 @@
+﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.Definitions;
+using Sandbox.Game;
+using Sandbox.ModAPI;
+using System;
+using System.Collections.Generic;
+using VRage.Game;
+using VRage.Game.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+using VRageMath;
+
+namespace GrappleHook
+{
+	public class Tools
+	{
+		public const float Tick = 1f / 60f;
+		public const float MillisecondPerFrame = 1000f / 60f;
+		public const float FireRateMultiplayer = 1f / 60f / 60f;
+
+		public const float MinutesToMilliseconds = 1f / 60f / 1000f;
+		public const float TicksToMilliseconds = 1f / TimeSpan.TicksPerMillisecond;
+
+		public static Random Random = new Random();
+
+		public static T CastProhibit<T>(T ptr, object val) => (T)val;
+
+		public static float MaxSpeedLimit => ((MyDefinitionManager.Static.EnvironmentDefinition.LargeShipMaxSpeed > MyDefinitionManager.Static.EnvironmentDefinition.SmallShipMaxSpeed) ?
+			MyDefinitionManager.Static.EnvironmentDefinition.LargeShipMaxSpeed : MyDefinitionManager.Static.EnvironmentDefinition.SmallShipMaxSpeed) + 10;
+
+		public static bool DebugMode = false;
+		private const string Prefix = "[GrappleHook] ";
+
+		public static void Info(string message)
+		{
+			MyLog.Default.Info($"{Prefix}{message}");
+		}
+
+		public static void Error(string message)
+		{
+			MyLog.Default.Error($"{Prefix}{message}");
+		}
+
+		public static void Warning(string message)
+		{
+			MyLog.Default.Warning($"{Prefix}{message}");
+		}
+
+		public static void Debug(string message)
+		{
+			if (DebugMode)
+			{
+				MyLog.Default.Info($"[DEBUG] {Prefix}{message}");
+			}
+		}
+
+		public static float GetScalerInverse(float mult)
+		{
+			if (mult > 1)
+			{
+				mult = 1 / mult;
+			}
+			else
+			{
+				mult = 1 + (1 - mult);
+			}
+			return mult;
+		}
+
+		public static double AngleBetween(Vector3D norm1, Vector3D norm2)
+		{
+			float ratio = Vector3.Dot(norm1, norm2);
+
+			double theta;
+
+			if (ratio < 0)
+			{
+				theta = Math.PI - 2.0 * Math.Asin((-norm1 - norm2).Length() / 2.0);
+			}
+			else
+			{
+				theta = 2.0 * Math.Asin((norm1 - norm2).Length() / 2.0);
+			}
+
+			return theta * 180 / Math.PI;
+		}
+
+		private const int Seed = 5366354;
+		private static float[] RandomSet;
+		private static float[] RandomSetFromAngle;
+		public static Vector3 ApplyDeviation(Vector3 direction, float maxAngle, ref sbyte index)
+		{
+			if (maxAngle == 0)
+				return direction;
+
+			if (RandomSet == null)
+			{
+				RandomSet = new float[128];
+				RandomSetFromAngle = new float[128];
+
+				Random rand = new Random(Seed);
+
+				for (int i = 0; i < 128; i++)
+				{
+					RandomSet[i] = (float)(rand.NextDouble() * Math.PI * 2);
+				}
+
+				for (int i = 0; i < 128; i++)
+				{
+					RandomSetFromAngle[i] = (float)rand.NextDouble();
+				}
+			}
+
+			if (index == 127)
+			{
+				index = 0;
+			}
+			else
+			{
+				index++;
+			}
+
+			Matrix matrix = Matrix.CreateFromDir(direction);
+
+			float randomFloat = (RandomSetFromAngle[index] * maxAngle * 2) - maxAngle;
+			float randomFloat2 = RandomSet[index];
+
+			Vector3 normal = -new Vector3(MyMath.FastSin(randomFloat) * MyMath.FastCos(randomFloat2), MyMath.FastSin(randomFloat) * MyMath.FastSin(randomFloat2), MyMath.FastCos(randomFloat));
+			return Vector3.TransformNormal(normal, matrix);
+		}
+		public static SerializableDefinitionId GetSelectedHotbarDefinition(IMyShipController cockpit)//, ref int index)
+		{
+			if (cockpit == null)
+				return default(SerializableDefinitionId);
+
+			MyObjectBuilder_ShipController builder = cockpit.GetObjectBuilderCubeBlock(false) as MyObjectBuilder_ShipController;
+
+			int? slotIndex = builder?.Toolbar?.SelectedSlot;
+			if (!slotIndex.HasValue)
+				return default(SerializableDefinitionId);
+
+			MyObjectBuilder_Toolbar toolbar = builder.Toolbar;
+			if (toolbar.Slots.Count <= slotIndex.Value)
+				return default(SerializableDefinitionId);
+
+			var item = toolbar.Slots[slotIndex.Value];
+			if (!(item.Data is MyObjectBuilder_ToolbarItemWeapon))
+				return default(SerializableDefinitionId);
+
+			//index = toolbar.SelectedSlot.Value;
+			return (item.Data as MyObjectBuilder_ToolbarItemWeapon).defId;
+		}
+		public static float NormalizeAngle(int angle)
+		{
+			int num = angle % 360;
+			if (num == 0 && angle != 0)
+			{
+				return 360f;
+			}
+			return num;
+		}
+
+
+		private static Dictionary<long, IMyGps> _gpsPoints = new Dictionary<long, IMyGps>();
+		public static void AddGPS(long gridId, Vector3D target)
+		{
+			if (!_gpsPoints.ContainsKey(gridId))
+			{
+				_gpsPoints.Add(gridId, MyAPIGateway.Session.GPS.Create(gridId.ToString(), "", target, true));
+				MyAPIGateway.Session.GPS.AddLocalGps(_gpsPoints[gridId]);
+				MyVisualScriptLogicProvider.SetGPSColor(gridId.ToString(), Color.Orange);
+				_gpsPoints[gridId].Name = "";
+			}
+
+			_gpsPoints[gridId].Coords = target;
+		}
+
+		public static void ClearGPS()
+		{
+			//if (!_wasInTurretLastFrame)
+			//	return;
+
+			foreach (IMyGps gps in _gpsPoints.Values)
+			{
+				MyAPIGateway.Session.GPS.RemoveLocalGps(gps);
+			}
+			_gpsPoints.Clear();
+
+			//_wasInTurretLastFrame = false;
+		}
+
+		public static void RemoveGPS(long id)
+		{
+			if (_gpsPoints.ContainsKey(id))
+			{
+				MyAPIGateway.Session.GPS.RemoveLocalGps(_gpsPoints[id]);
+				_gpsPoints.Remove(id);
+			}
+		}
+
+	}
+}
---
+++ GrappleHook/Data/Scripts/hook/WeaponControlLayer.cs
@@ -0,0 +1,302 @@
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game.Weapons;
+using Sandbox.Gui;
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Interfaces.Terminal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+using VRageMath;
+using static VRageRender.MyBillboard;
+
+namespace GrappleHook
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_LargeMissileTurret), true, "LargeCalibreTurret")]
+    /// <summary>
+    /// Binds keens interface to the custom weapon types
+    /// </summary>
+    public class WeaponControlLayer : MyGameLogicComponent
+    {
+        public static bool Hijack = false;
+
+        private bool waitframe = true;
+
+        private IMyLargeTurretBase Turret;
+        private IMyGunObject<MyGunBase> gun;
+
+        private IMyEntity connectedEntity = null;
+        private Vector3 localGrapplePosition = Vector3.Zero;
+        private double GrappleLength;
+
+        private bool GrappleActive = false;
+        private bool GrappleAnchered => connectedEntity != null;
+
+        private Vector3D GrapplePosition = Vector3D.Zero;
+        private Vector3 GrappleDirection = Vector3.Zero;
+        private float GrappleSpeed = 0.2f;
+
+        private double k = 20000000;
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            Tools.Debug("intitializing");
+            Tools.DebugMode = true;
+            gun = Entity as IMyGunObject<MyGunBase>;
+
+            Turret = Entity as IMyLargeTurretBase;
+            Turret.Range = 0;
+
+
+
+            if (Hijack) return;
+            NeedsUpdate |= MyEntityUpdateEnum.EACH_FRAME | MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+        }
+
+        public override void UpdateOnceBeforeFrame()
+        {
+            Tools.Debug("update before frame");
+            if (Hijack) return;
+
+            if (waitframe)
+            {
+                waitframe = false;
+                NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+                return;
+            }
+
+            OverrideDefaultControls<IMyLargeTurretBase>();
+            Hijack = true;
+        }
+
+        public override void UpdateBeforeSimulation()
+        {
+            if (Turret.IsShooting && !GrappleActive) 
+            {
+                Shoot();
+            }
+
+            if (!GrappleAnchered) 
+            {
+                UpdateProjectile();
+            }
+            else
+            {
+                Vector3D turretPostion = gun.GetMuzzlePosition();
+                Vector3D entityPostion = Vector3D.Transform(localGrapplePosition, connectedEntity.WorldMatrix);
+                Vector3D direction = turretPostion - entityPostion;
+                double currentLength = (direction).Length();
+                direction.Normalize();
+
+                double force = k * Math.Max(0, currentLength - GrappleLength);
+
+                Turret.CubeGrid.Physics.AddForce(MyPhysicsForceType.APPLY_WORLD_FORCE, -1 * direction * force, turretPostion, null);
+                connectedEntity.Physics.AddForce(MyPhysicsForceType.APPLY_WORLD_FORCE,  direction * force, entityPostion, null);
+
+                // apply grappling forces
+            }
+        }
+
+        public override void UpdateAfterSimulation()
+        {
+            if (MyAPIGateway.Utilities.IsDedicated) return;
+
+            Vector4 color = VRageMath.Color.DarkGray;
+
+            Vector3D position;
+            if (!GrappleAnchered)
+            {
+                position = GrapplePosition;
+                MySimpleObjectDraw.DrawLine(gun.GetMuzzlePosition(), position, MyStringId.GetOrCompute("cable"), ref color, 0.05f, BlendTypeEnum.Standard);
+            }
+            else 
+            {
+                position = Vector3D.Transform(localGrapplePosition, connectedEntity.WorldMatrix);
+
+
+
+            }
+        }
+
+        private void UpdateProjectile()
+        {
+            if (!GrappleActive) return;
+
+            Vector3 delta = GrappleDirection * GrappleSpeed;
+
+            IHitInfo hit = null;
+            MyAPIGateway.Physics.CastRay(GrapplePosition, GrapplePosition + delta, out hit);
+            if (hit != null && !hit.HitEntity.MarkedForClose)
+            {
+
+                connectedEntity = hit.HitEntity;
+                localGrapplePosition = Vector3D.Transform(hit.Position, MatrixD.Invert(connectedEntity.WorldMatrix));
+                GrappleLength = (hit.Position - gun.GetMuzzlePosition()).Length();
+                Tools.Debug($"GrappleLength: {GrappleLength}");
+
+
+                GrappleDirection = Vector3.Zero;
+                GrapplePosition = Vector3D.Zero;
+
+
+
+                Tools.Debug($"Hit entity: {hit.HitEntity.DisplayName}");
+                return;
+
+            }
+
+            GrapplePosition += delta;
+        }
+
+        public void Shoot()
+        {
+            Tools.Debug("Takeing a shot!");
+            Reset();
+            MatrixD muzzleMatrix = gun.GunBase.GetMuzzleWorldMatrix();
+            Vector3 direction = muzzleMatrix.Forward;
+            Vector3D origin = muzzleMatrix.Translation;
+
+            GrappleDirection = direction;
+            GrapplePosition = origin;
+            GrappleActive = true;
+
+        }
+
+        public void Reset() 
+        {
+            GrappleActive = false;
+
+            GrapplePosition = Vector3D.Zero;
+            GrappleDirection = Vector3.Zero;
+
+            connectedEntity = null;
+            localGrapplePosition = Vector3D.Zero;
+        }
+
+
+        private static void DisableAction(IMyTerminalAction a)
+        {
+            Func<IMyTerminalBlock, bool> oldEnabled = a.Enabled;
+            a.Enabled = (block) =>
+            {
+                WeaponControlLayer logic = block.GameLogic.GetAs<WeaponControlLayer>();
+                if (logic != null)
+                {
+                    return false;
+                }
+
+                return oldEnabled.Invoke(block);
+            };
+        }
+
+        private static void HideControl(IMyTerminalControl a)
+        {
+            Func<IMyTerminalBlock, bool> oldVisiable = a.Visible;
+            a.Visible = (block) =>
+            {
+                WeaponControlLayer logic = block.GameLogic.GetAs<WeaponControlLayer>();
+                if (logic != null)
+                {
+                    return false;
+                }
+
+                return oldVisiable.Invoke(block);
+            };
+        }
+
+        private static void OverrideDefaultControls<T>()
+        {
+            Tools.Debug("Made it to override controls!");
+            Action<IMyTerminalBlock> oldAction;
+            Action<IMyTerminalBlock, StringBuilder> oldWriter;
+            string[] bannedActions = new string[]
+            {
+                "IncreaseRange",
+                "DecreaseRange",
+                "EnableIdleMovement",
+                "EnableIdleMovement_On",
+                "EnableIdleMovement_Off",
+                "TargetMeteors",
+                "TargetMeteors_On",
+                "TargetMeteors_Off",
+                "TargetMissiles",
+                "TargetMissiles_On",
+                "TargetMissiles_Off",
+                "TargetSmallShips",
+                "TargetSmallShips_On",
+                "TargetSmallShips_Off",
+                "TargetLargeShips",
+                "TargetLargeShips_On",
+                "TargetLargeShips_Off",
+                "TargetCharacters",
+                "TargetCharacters_On",
+                "TargetCharacters_Off",
+                "TargetStations",
+                "TargetStations_On",
+                "TargetStations_Off",
+                "TargetNeutrals",
+                "TargetNeutrals_On",
+                "TargetNeutrals_Off",
+                "TargetFriends",
+                "TargetFriends_On",
+                "TargetFriends_Off",
+                "TargetEnemies",
+                "TargetEnemies_On",
+                "TargetEnemies_Off",
+                "EnableTargetLocking",
+                "TargetingGroup_Weapons",
+                "TargetingGroup_Propulsion",
+                "TargetingGroup_PowerSystems",
+                "TargetingGroup_CycleSubsystems",
+                "FocusLockedTarget",
+            };
+
+
+            Func<IMyTerminalBlock, bool> oldGetter;
+            Action<IMyTerminalBlock, bool> oldSetter;
+
+            string[] bannedControls = new string[]
+            {
+                "Range",
+                "EnableIdleMovement",
+                "TargetMeteors",
+                "TargetMissiles",
+                "TargetSmallShips",
+                "TargetLargeShips",
+                "TargetCharacters",
+                "TargetStations",
+                "TargetNeutrals",
+                "TargetFriends",
+                "TargetEnemies",
+                "EnableTargetLocking",
+                "TargetingGroup_Selector",
+                "FocusLockedTarget",
+            };
+
+            List<IMyTerminalAction> actions = new List<IMyTerminalAction>();
+            MyAPIGateway.TerminalControls.GetActions<T>(out actions);
+            foreach (IMyTerminalAction a in actions)
+            {
+                if (bannedActions.Contains(a.Id))
+                {
+                    DisableAction(a);
+                }
+            }
+
+            List<IMyTerminalControl> controls = new List<IMyTerminalControl>();
+            MyAPIGateway.TerminalControls.GetControls<T>(out controls);
+            foreach (IMyTerminalControl c in controls)
+            {
+                if (bannedControls.Contains(c.Id))
+                {
+                    HideControl(c);
+                }
+            }
+        }
+    }
+}
