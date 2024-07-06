60d4099a5ae7a17487d97cba744c76699098c660
Gauge <Gauger16@gmail.com>
2024-07-06 14:39:51 -0700

    turned off logging

--- GrappleHook/Data/Scripts/hook/Core.cs
+++ GrappleHook/Data/Scripts/hook/Core.cs
@@ -13,51 +13,23 @@ using VRageMath;
 
 namespace GrappleHook
 {
-	[MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation)]
+	[MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
 	public class Core : MySessionComponentBase
 	{
 		public const ushort ModId = 32144;
 		public const string ModName = "GrappleHook";
 
-        public static double raycastDistance = 0;
-
 
         public override void Init(MyObjectBuilder_SessionComponent sessionComponent)
 		{
-            Tools.DebugMode = true;
-            NetworkAPI.LogNetworkTraffic = true;
+            Tools.DebugMode = false;
+            NetworkAPI.LogNetworkTraffic = false;
 
 			if (!NetworkAPI.IsInitialized)
 			{
 				NetworkAPI.Init(ModId, ModName);
 			}
 		}
-
-        //public override void UpdateBeforeSimulation()
-        //{
-        //    if (MyAPIGateway.Utilities.IsDedicated) return;
-        //    if (MyAPIGateway.Session.Player?.Character == null) return;
-
-        //    IMyCamera cam = MyAPIGateway.Session.Camera;
-        //    if (cam == null) return;
-
-        //    Vector3D position = cam.WorldMatrix.Translation;
-        //    Vector3D distance = cam.WorldMatrix.Forward * 2.5f;
-
-        //    Vector3D endPosition = position + distance;
-
-        //    IHitInfo hit = null;
-        //    MyAPIGateway.Physics.CastRay(cam.WorldMatrix.Translation, position + distance, out hit);
-        //    if (hit != null)
-        //    {
-        //        endPosition = hit.Position;
-        //        raycastDistance = 7.5f * hit.Fraction;
-        //    }
-        //    else 
-        //    {
-        //        raycastDistance = 7.5f;
-        //    }
-        //}
     }
 }
 
