e775f9ec74eb3e75211b7e40270ef186d78c1a08
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-07-08 03:33:59 -0500

    Update Settings.cs

--- ThermalDynamics/Data/Scripts/Thermodynamics/Settings.cs
+++ ThermalDynamics/Data/Scripts/Thermodynamics/Settings.cs
@@ -73,7 +73,7 @@ namespace Thermodynamics
 			Settings s = new Settings {
 				Version = 1,
 				EnableSolarHeat = true,
-				EnableDamage = false,
+				EnableDamage = true,
 				Frequency = 60,
 				SimulationSpeed = 1,
                 SolarEnergy = 1000f, 
