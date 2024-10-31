d036b9005eeef93cd487bc84ff307ad08b187370
Gauge <Gauger16@gmail.com>
2024-10-31 00:33:57 -0700

    Added coolant pipes

---
+++ ThermalDynamics/Data/Scripts/Thermodynamics/Blocks/CoolantPipe.cs
@@ -0,0 +1,28 @@
+﻿using Sandbox.Definitions;
+using Sandbox.ModAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+
+namespace Thermodynamics
+{
+
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_CubeBlock), true, "Gauge_LG_CoolantPipe_Straight")]
+    public class CoolantPipe : MyGameLogicComponent
+    {
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            IMyCubeBlock block = (this.Entity as IMyCubeBlock);
+            MyCubeBlockDefinition def = block.SlimBlock.BlockDefinition as MyCubeBlockDefinition;
+
+            MyLog.Default.Info("TEST!!!!! " + def.MountPoints.Count().ToString());
+        }
+    }
+}
